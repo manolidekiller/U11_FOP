3290841df619980551d37724579da876046a2af2
manolidekiller <emmanouil.vergopoulos@gmail.com>
2017-01-24 10:57:51 +0100

    FOPU11 Erste Version

---
+++ src/hospital/heap/ArrayListHeap.java
@@ -0,0 +1,49 @@
+package hospital.heap;
+
+public class ArrayListHeap extends AbstractBinaryHeap {
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean isHeap(int parentIndex, int childIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void swapNodes(int parentIndex, int childIndex) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void push(Comparable element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Comparable top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+
+}
---
+++ src/hospital/test/TestPatientQueue.java
@@ -0,0 +1,83 @@
+package hospital.test;
+
+
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import org.junit.Test;
+
+import hospital.patients.AbstractPatient;
+
+public class TestPatientQueue {
+	
+
+	//**************************************************************
+	//********************** Helper method *************************
+	//**************************************************************	
+	/**
+	 * Takes an array of patients and returns a shuffled copy.
+	 * @param patients the patients
+	 * 
+	 * @return the patients in shuffled order
+	 */
+	private static AbstractPatient[] getShuffledArray(AbstractPatient[] patients){
+		// Init random number generator with fixed seed
+		Random random = new Random(12345);
+		
+		// Add patients to new list
+		ArrayList<AbstractPatient> list = new ArrayList<AbstractPatient>();
+		for (AbstractPatient p : patients){
+			list.add(p);
+		}		
+		
+		AbstractPatient[] shuffledArray = new AbstractPatient[list.size()];
+		Collections.shuffle(list, random);
+		list.toArray(shuffledArray);
+		return shuffledArray;
+	}
+	
+
+	//**************************************************************
+	//****************** Test patient categories *******************
+	//**************************************************************	
+	
+	// Check correct behavior of the getPriority() method 
+	// for each patient class.
+	@Test
+	public void validateGetPriority(){
+		//TODO Implement test!
+	}
+	
+	// Check correct behavior of the getCatInfo() method
+	// for each patient class.
+	@Test
+	public void validateGetCatInfo(){
+		//TODO Implement test!
+	}
+	
+	// Check correct behavior of the compareTo(...) method WITHIN each patient
+	// class, i.e. check ordering for each category separately. Use at least
+	// three object instances per patient class!
+	@Test
+	public void validateCompareTo(){
+		//TODO Implement test!
+	}
+
+	//**************************************************************
+	//********************* Test patient queue *********************
+	//**************************************************************
+	
+	// Check correct behavior of the process method of the patient queue class,
+	// i.e. patients are processed in correct order. Initialize test by adding
+	// patients in random order. To generate a randomly ordered patient list, 
+	// you can use the provided getShuffledArray(AbstractPatient[] patients) method.
+	@Test
+	public void validateProcessQueue(){
+		//TODO Implement test!
+	}
+	
+	
+}
