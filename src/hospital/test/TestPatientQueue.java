782dea5b011f23f47c25a09f476d34f19692b5c2
Ozan Agtas <ozan.agtas@gmail.com>
2017-01-25 13:05:16 +0100

    Abgabe

--- src/hospital/test/TestPatientQueue.java
+++ src/hospital/test/TestPatientQueue.java
@@ -83,8 +83,7 @@ public class TestPatientQueue {
 		//Test Severely Injured test patients
 		SeverelyInjuredPatient siPatient1 = new SeverelyInjuredPatient("TestSeverelyInjured1", LocalTime.of(10, 30), 5);
 		SeverelyInjuredPatient siPatient2 = new SeverelyInjuredPatient("TestSeverelyInjured2", LocalTime.of(20, 30), 5);
-		SeverelyInjuredPatient siPatient3 = new SeverelyInjuredPatient("TestSeverelyInjured3", LocalTime.of(20, 30), 10);
-		
+		SeverelyInjuredPatient siPatient3 = new SeverelyInjuredPatient("TestSeverelyInjured3", LocalTime.of(20, 30), 10);		
 		
 	     //normal test objects
 		SlightlyInjuredPatient patient1 = new SlightlyInjuredPatient("TestSlightlyInjrued1", LocalTime.of(19, 30));
@@ -95,28 +94,19 @@ public class TestPatientQueue {
 		assertEquals(-1, emergencyPatient2.compareTo(emergencyPatient3));
 		assertEquals(0, emergencyPatient1.compareTo(emergencyPatient1));
 		
-		
-		
 		assertEquals(1, siPatient1.compareTo(siPatient3));
 		assertEquals(-1, siPatient3.compareTo(siPatient2));
 		assertEquals(0, siPatient3.compareTo(siPatient3));
-		
-		
+			
 		assertEquals(-1, patient1.compareTo(patient2));
 		assertEquals(1, patient3.compareTo(patient1));
 		assertEquals(0, patient2.compareTo(patient2));
-		
-		
 		
 	    assertEquals(-1, emergencyPatient1.compareTo(patient3));
 	    assertEquals(-1, siPatient2.compareTo(patient3));
 	    assertEquals(1, patient1.compareTo(emergencyPatient3));
 	    assertEquals(1, patient2.compareTo(emergencyPatient1));
 	    assertEquals(1, siPatient3.compareTo(emergencyPatient1));
-		
-		
-		
-	
 	}
 
 	//**************************************************************
@@ -136,22 +126,22 @@ public class TestPatientQueue {
 		EmergencyPatient hard2 = new EmergencyPatient("Dwayne TheRock Johnson", LocalTime.of(10, 32));
 		AbstractPatient[] listOfPatients = { easy1, med1, med2, hard1, hard2 };
 		listOfPatients = getShuffledArray(listOfPatients);
-		
 		PatientQueue<AbstractPatient> list = new PatientQueue<AbstractPatient>();
-		
 		for (AbstractPatient abstractPatient : (listOfPatients)) {
 			System.out.println(abstractPatient.toString());
 			list.addPatient(abstractPatient);
 		}
-		
-		System.out.println("foreach fertig. Process Queue startet");
 		ArrayList<String> result = list.processQueue();
-		System.out.println("Process Queue fertig");
+		System.out.println("Ergebnis von ProcessQueue():");
 		for (String string : result) {
 			System.out.println(string);
 		}
+		ArrayList<String> expectedList = new ArrayList<String>();
+		expectedList.add("T1/Red            -> 10:30 - Mustermann");
+		expectedList.add("T1/Red            -> 10:32 - Dwayne TheRock Johnson");
+		expectedList.add("T2/Orange-51      -> 14:51 - Salz");
+		expectedList.add("T2/Orange-12      -> 23:23 - Max");
+		expectedList.add("T3/Green          -> 17:15 - Josef");
+		assertArrayEquals(expectedList.toArray(), result.toArray());
 	}
-	
-	
-	
 }
