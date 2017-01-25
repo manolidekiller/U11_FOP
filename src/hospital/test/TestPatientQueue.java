a7a14df22895fa520e3ed395a985b7ee757ef501
manolidekiller <emmanouil.vergopoulos@gmail.com>
2017-01-25 12:54:31 +0100

    Meine Tests hinzugefuegt
    geschafft xd

--- src/hospital/test/TestPatientQueue.java
+++ src/hospital/test/TestPatientQueue.java
@@ -1,5 +1,7 @@
 package hospital.test;
 
+import static org.junit.Assert.*;
+
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -38,7 +40,6 @@ public class TestPatientQueue {
 		return shuffledArray;
 	}
 	
-
 	//**************************************************************
 	//****************** Test patient categories *******************
 	//**************************************************************	
@@ -47,14 +48,26 @@ public class TestPatientQueue {
 	// for each patient class.
 	@Test
 	public void validateGetPriority(){
-		//TODO Implement test!
+		EmergencyPatient testpatient1 = new EmergencyPatient("TestEmergency", LocalTime.of(20, 30));
+		SeverelyInjuredPatient testpatient2 = new SeverelyInjuredPatient("TestSeverlyInjured", LocalTime.of(20, 30), 5);
+		SlightlyInjuredPatient testpatient3 = new SlightlyInjuredPatient("TestSlightlyInjured", LocalTime.of(20, 30));
+		assertSame(TreatmentPriority.HIGH, testpatient1.getPriority());
+		assertSame(TreatmentPriority.MEDIUM, testpatient2.getPriority());
+		assertSame(TreatmentPriority.LOW, testpatient3.getPriority());
+         
 	}
 	
 	// Check correct behavior of the getCatInfo() method
 	// for each patient class.
 	@Test
 	public void validateGetCatInfo(){
-		//TODO Implement test!
+		EmergencyPatient testpatient1 = new EmergencyPatient("TestEmergency", LocalTime.of(20, 30));
+		SeverelyInjuredPatient testpatient2 = new SeverelyInjuredPatient("TestSeverlyInjured", LocalTime.of(20, 30), 5);
+		SlightlyInjuredPatient testpatient3 = new SlightlyInjuredPatient("TestSlightlyInjured", LocalTime.of(20, 30));
+	
+		assertEquals("T1/Red" ,testpatient1.getCatInfo());
+		assertEquals(String.format("T2/Orange-%d", 5), testpatient2.getCatInfo());
+		assertEquals("T3/Green",testpatient3.getCatInfo());
 	}
 	
 	// Check correct behavior of the compareTo(...) method WITHIN each patient
@@ -62,7 +75,48 @@ public class TestPatientQueue {
 	// three object instances per patient class!
 	@Test
 	public void validateCompareTo(){
-		//TODO Implement test!
+		// Emergency Patients test objects
+		EmergencyPatient  emergencyPatient1 = new EmergencyPatient("TestEmergency1", LocalTime.of(20, 30));
+		EmergencyPatient  emergencyPatient2 = new EmergencyPatient("TestEmergency2", LocalTime.of(19, 15));
+		EmergencyPatient  emergencyPatient3 = new EmergencyPatient("TestEmergency3", LocalTime.of(21, 10));
+		
+		//Test Severely Injured test patients
+		SeverelyInjuredPatient siPatient1 = new SeverelyInjuredPatient("TestSeverelyInjured1", LocalTime.of(10, 30), 5);
+		SeverelyInjuredPatient siPatient2 = new SeverelyInjuredPatient("TestSeverelyInjured2", LocalTime.of(20, 30), 5);
+		SeverelyInjuredPatient siPatient3 = new SeverelyInjuredPatient("TestSeverelyInjured3", LocalTime.of(20, 30), 10);
+		
+		
+	     //normal test objects
+		SlightlyInjuredPatient patient1 = new SlightlyInjuredPatient("TestSlightlyInjrued1", LocalTime.of(19, 30));
+		SlightlyInjuredPatient patient2 = new SlightlyInjuredPatient("TestSlightlyInjured2", LocalTime.of(20, 30));
+		SlightlyInjuredPatient patient3 = new SlightlyInjuredPatient("TestSlightlyInjured3", LocalTime.of(21, 30));
+		
+		assertEquals(1, emergencyPatient1.compareTo(emergencyPatient2));
+		assertEquals(-1, emergencyPatient2.compareTo(emergencyPatient3));
+		assertEquals(0, emergencyPatient1.compareTo(emergencyPatient1));
+		
+		
+		
+		assertEquals(1, siPatient1.compareTo(siPatient3));
+		assertEquals(-1, siPatient3.compareTo(siPatient2));
+		assertEquals(0, siPatient3.compareTo(siPatient3));
+		
+		
+		assertEquals(-1, patient1.compareTo(patient2));
+		assertEquals(1, patient3.compareTo(patient1));
+		assertEquals(0, patient2.compareTo(patient2));
+		
+		
+		
+	    assertEquals(-1, emergencyPatient1.compareTo(patient3));
+	    assertEquals(-1, siPatient2.compareTo(patient3));
+	    assertEquals(1, patient1.compareTo(emergencyPatient3));
+	    assertEquals(1, patient2.compareTo(emergencyPatient1));
+	    assertEquals(1, siPatient3.compareTo(emergencyPatient1));
+		
+		
+		
+	
 	}
 
 	//**************************************************************
